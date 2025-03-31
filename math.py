44b842d1fa30af51f96d01676f2f63c5da6f6278
MuhmmadAyman <muhammad24ai@drttit.edu.in>
2025-03-31 23:08:38 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,8 @@
+import math 
+n=int(input("enter the number: "))
+squareroot=math.sqrt(n)
+logarithm=math.log(n)
+sine=math.sin(n)
+print(f"Square root:{squareroot}")
+print(f"logarithm:{logarithm}")
+print(f"sine:{sine}")
